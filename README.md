66eda9dd41bcd5a999e080a9fc781c7d70137200
Jan Nielsen <jan.sture.nielsen@gmail.com>
2021-03-21 11:54:37 -0600

    Initial commit of largely mostly Spring Boot's Greeting sample.

---
+++ README.md
@@ -0,0 +1,2 @@
+The Spring Boot Greeting sample split into two Spring Boot applications, a client and a server, to test basic connection establishment and performance.
+
---
+++ client/src/main/java/ws/stomp/client/Application.java
@@ -0,0 +1,119 @@
+package ws.stomp.client;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+	private final int port = 8901;
+
+	private SockJsClient sockJsClient;
+
+	private WebSocketStompClient stompClient;
+
+	private final WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+
+	public static void main(String[] args) throws InterruptedException, AssertionError {
+		SpringApplication.run(Application.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+		List<Transport> transports = new ArrayList<>();
+		transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+		sockJsClient = new SockJsClient(transports);
+
+		stompClient = new WebSocketStompClient(sockJsClient);
+		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+		final CountDownLatch latch = new CountDownLatch(1);
+		final AtomicReference<Throwable> failure = new AtomicReference<>();
+
+		StompSessionHandler handler = new TestSessionHandler(failure) {
+
+			@Override
+			public void afterConnected(final StompSession session, StompHeaders connectedHeaders) {
+				session.subscribe("/topic/greetings", new StompFrameHandler() {
+					@Override
+					public Type getPayloadType(StompHeaders headers) {
+						return Greeting.class;
+					}
+
+					@Override
+					public void handleFrame(StompHeaders headers, Object payload) {
+						Greeting greeting = (Greeting) payload;
+						try {
+							System.out.println("Greeting: " + greeting.getContent());
+						} catch (Throwable t) {
+							failure.set(t);
+						} finally {
+							session.disconnect();
+							latch.countDown();
+						}
+					}
+				});
+				try {
+					session.send("/app/hello", new HelloMessage("Spring"));
+				} catch (Throwable t) {
+					failure.set(t);
+					latch.countDown();
+				}
+			}
+		};
+
+		stompClient.connect("ws://localhost:{port}/echo", headers, handler, port);
+
+		if (latch.await(3, TimeUnit.MINUTES/*.SECONDS*/)) {
+			if (failure.get() != null) {
+				throw new AssertionError("", failure.get());
+			}
+		} else {
+			System.err.println("Greeting not received");
+		}
+	}
+
+	private class TestSessionHandler extends StompSessionHandlerAdapter {
+
+		private final AtomicReference<Throwable> failure;
+
+		public TestSessionHandler(AtomicReference<Throwable> failure) {
+			this.failure = failure;
+		}
+
+		@Override
+		public void handleFrame(StompHeaders headers, Object payload) {
+			this.failure.set(new Exception(headers.toString()));
+		}
+
+		@Override
+		public void handleException(StompSession s, StompCommand c, StompHeaders h, byte[] p, Throwable ex) {
+			this.failure.set(ex);
+		}
+
+		@Override
+		public void handleTransportError(StompSession session, Throwable ex) {
+			this.failure.set(ex);
+		}
+	}
+}
